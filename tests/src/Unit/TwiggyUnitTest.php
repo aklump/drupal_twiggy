02f1a64fa0e60186938d3a528d51273d401df861
Aaron Klump <sourcecode@intheloftstudios.com>
2019-02-11 16:14:08 -0800

    Refactor _twiggy_is_attributes_string to address false negative.

---
+++ tests/src/Unit/TwiggyUnitTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\Tests\twiggy;
+
+use AKlump\DrupalTest\UnitTestBase;
+use Drupal\twiggy\Twiggy;
+
+/**
+ * Test coverage for Twiggy.
+ *
+ * @group loft
+ * @coversDefaultClass Twiggy;
+ * @SuppressWarnings(PHPMD.StaticAccess)
+ * @SuppressWarnings(PHPMD.TooManyPublicMethods)
+ */
+class TwiggyFunctionsUnitTest extends UnitTestBase {
+
+  /**
+   * Define the class being tested, it's arguments and mock objects needed.
+   *
+   * @var array
+   *   The schema array for this test.
+   */
+  protected function getSchema() {
+    return [
+      'classToBeTested' => FALSE,
+    ];
+  }
+
+  /**
+   * Provides data for test_twiggy_is_attributes_string.
+   */
+  public function dataForTest_twiggy_is_attributes_stringProvider() {
+    $tests = array();
+    $tests[] = array(
+      'Sometimes strings are just strings',
+      FALSE,
+    );
+    $tests[] = array(
+      'class="t-explore-stories"',
+      FALSE,
+    );
+    $tests[] = array(
+      ' class="t-explore-stories" data-autofocus data-amplitude="{&quot;do&quot;:&quot;re&quot;,&quot;mi&quot;:&quot;fa&quot;}" href="/library"',
+      TRUE,
+    );
+    $tests[] = array(
+      ' class="red-color" style="background-color: #ffff00;',
+      TRUE,
+    );
+    $tests[] = array(
+      ' class="red-color"',
+      TRUE,
+    );
+
+    return $tests;
+  }
+
+  /**
+   * @dataProvider dataForTest_twiggy_is_attributes_stringProvider
+   */
+  public function test_twiggy_is_attributes_string($subject, $control) {
+    $this->assertSame($control, _twiggy_is_attributes_string($subject));
+  }
+
+}
